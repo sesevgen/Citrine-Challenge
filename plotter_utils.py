904372ac108d4f58dbd53e7f09e18c0e9974acee
Emre <sesevgen@gmail.com>
2019-04-18 16:02:15 -0500

    Close to finished. Needs testing and cleanup.

---
+++ plotter_utils.py
@@ -0,0 +1,62 @@
+import argparse
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def parse_input_plotter():
+	"""
+	Parses inputs to the plotter function, performing some common sense asserts and returns the output file to read from and the dimensions to plot.
+	"""
+
+	parser = argparse.ArgumentParser(description='This script plots generated vectors using a subset of their dimensions, in 1D, 2D or 3D as a way to quickly check point spread. Many localized clusters indicates poor sampling, whereas well distibuted points indicates good sampling.')
+	parser.add_argument('output_file',help='Name of the file containing generated vectors.')
+	parser.add_argument('dimensions',help='Dimensions to plot. Should have 1, 2 or 3 integers.',nargs='+')
+	
+	args = parser.parse_args()
+	output_file = args.output_file
+	try:
+		dimensions = [int(dim) for dim in args.dimensions]
+	except TypeError:
+		print('Please provide integers for dimensions')
+
+	assert ((len(dimensions) <= 3) and (len(dimensions) >= 0)), "Please provide 1, 2 or 3 integers for dimension"
+	assert os.path.isfile(output_file), "File not found."
+
+	print('Plotting vectors from '+output_file+' in the following dimension(s):',dimensions)	
+
+	return output_file, dimensions
+
+
+def load_array(output_file,dimensions):
+	"""
+	Returns a numpy array with the desired dimensions, read from provided file.
+	"""
+	fullarray = np.loadtxt(output_file)
+	array = np.zeros((fullarray.shape[0],len(dimensions)))
+	for i, dim in enumerate(dimensions):
+		array[:,i] = fullarray[:,dim]
+
+	return array
+
+def plot_1d(array,dimensions):
+	plt.figure()
+	plt.plot(array)
+	plt.xlabel(str(dimensions[0])+'th Dimension')
+	plt.show()
+
+def plot_2d(array,dimensions):
+	plt.figure()
+	plt.plot(array[:,0],array[:,1])
+	plt.xlabel(str(dimensions[0])+'th Dimension')
+	plt.ylabel(str(dimensions[1])+'th Dimension')
+	plt.show()
+
+def plot_3d(array,dimensions):
+	plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.scatter3D(array[:,0],array[:,1],array[:,2])
+	ax.set_xlabel(str(dimensions[0])+'th Dimension')
+	ax.set_ylabel(str(dimensions[1])+'th Dimension')
+	ax.set_zlabel(str(dimensions[2])+'th Dimension')
+	plt.show()
---
+++ utils.py
@@ -0,0 +1,75 @@
+import argparse
+import os.path
+import numpy as np
+
+def parse_input_sampler():
+	"""
+	Parses inputs to the sampler function, performing some common sense asserts and returns the input file to read from, output file to write to, and the number of points to generate.
+	"""
+
+	parser = argparse.ArgumentParser(description='This script generates vectors in n-dimensional space within the unit hypercube subject to constraints provided as python expressions from an input file. Requires a seed point, ideally within the constraints.')
+
+
+	parser.add_argument('input_file', help='Name of the input file containing dimensionality and constraints')
+	parser.add_argument('output_file', help='Name of the output file to export generated points to')
+	parser.add_argument('n_results', help='Number of points to generate', type=int)
+	parser.add_argument('-f','--format', nargs='?', default = '%10.10f', help='Formatting of the output. Default = %%10.10f')
+	parser.add_argument('-d','--decay', nargs='?', default = 0.99, type=float, 
+		help='Decay of random walk step size per failure. Should be between 0 and 1. Higher values slow point generation but increase point spread and chance of ergodicity. Default = 0.99')
+	parser.add_argument('-s','--steps',nargs='?', default = 20, type=int, 
+		help='Number of random walk steps to attempt per point. Higher values speed up point generation but also cause points to be more clustered. Default = 20')
+	parser.add_argument('-r','--reset',nargs='?', default = 'zero',
+		help='Options: zero or decrement - Mode to reset random walk step length. \'zero\' resets random walk step length on a success. \'decrement\' decrements failure counter by 1. \'decrement\' is significantly faster, but also generates significantly more correlated samples. Default = \'zero\' ')
+
+
+	args = parser.parse_args()
+	input_file = args.input_file
+	assert os.path.isfile(input_file), "File not found."
+	output_file = args.output_file
+	n_results = args.n_results
+	assert args.reset == 'zero' or args.reset == 'decrement' 
+	optional_args={ 'format': args.format,
+			'decay' : args.decay,
+			'steps': args.steps,
+			'reset': args.reset}
+
+
+	print(('Generating {} points, subject to constraints from '+input_file+', and writing the results to '+output_file+'.').format(n_results))
+
+	return input_file, output_file, n_results, optional_args
+
+def write_output(output_file,container,fmt='%10.10f'):
+	try:
+		np.savetxt(output_file,np.array(container),fmt=fmt)
+	except ValueError:
+		print('Error reading format. Defaulting to standard formatting of %10.10f')
+		np.savetxt(output_file,np.array(container),fmt='%10.10f')
+
+	print('Results comprised of {} vectors written to '.format(len(container))+output_file)
+
+def run_check(container,constraints):
+	"""
+	Checks that all vectors in the container obey constraints and are within the unit hypercube. Returns a boolean and a list of failed points, if any.
+	"""
+
+	print('Running final check on generated vectors, ensuring all are within the unit hypercube and obey constraints.')
+	
+	passed = True
+
+	failures = []
+	for i, vector in enumerate(container):
+		for coord in vector:
+			if coord < 0.0 or coord > 1.0:
+				failures.append(i)
+				passed = False
+		if not constraints.apply(vector):
+			failures.append(i)
+			passed = False
+
+	if passed:
+		print('Final check completed successfully. All points are within the unit hypercube and obey constraints.')
+	
+	else:
+		print('Final check failed. Failure at entries: ',failures)
+
+	return passed, failures		
